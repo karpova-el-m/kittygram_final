364594deb93d312836dfaffd3abf11075e44c1e3
Елена Карпова <karpova.el.m@gmail.com>
2024-11-08 00:20:59 +0100

    7/11 check

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -4,11 +4,16 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
+SECRET_KEY = os.getenv('SECRET_KEY', '')
 
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'True')
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '84.252.136.172', 'kittygramhomework.zapto.org']
+ALLOWED_HOSTS = [
+    os.getenv('ALLOWED_HOST_1', ''),
+    os.getenv('ALLOWED_HOST_2', ''),
+    os.getenv('ALLOWED_HOST_3', ''),
+    os.getenv('ALLOWED_HOST_4', '')
+]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -94,8 +99,6 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
 
 MEDIA_URL = '/media/'
-
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_ROOT = '/media/'
 
